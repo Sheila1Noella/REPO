0ecc1cc48153b44f27a830639c92613a20ddf6e3
Sheila1Noella <asheilanoela@gmail.com>
2021-01-07 21:33:27 +0200

    First to do

---
+++ Todo_App/tasks1.js
@@ -0,0 +1,51 @@
+var Mylist = document.getElementsByTagName('li');
+var i;
+for(i = 0; i < Mylist.length; i++){
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("");
+    span.className = "close";
+    span.appendChild(txt);
+    Mylist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+var j;
+for(j = 0; j < close.length; j++){
+    close[j].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+var list = document.querySelector('');
+list.addEventListener('click', function(ev){
+    if(ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if(inputValue === ''){
+        alert("You must write something!");
+    }
+    else{
+        document.getElementById('myUL').appendChild(li);
+    }
+    document.getElementById('myInput').value ="";
+
+var span = document.createElement("SPAN");
+var txt = document.createTextNode("");
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+
+for(l = 0; l < close.length; l++){
+    close[l].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = 'none';
+    }
+}
+}
+
